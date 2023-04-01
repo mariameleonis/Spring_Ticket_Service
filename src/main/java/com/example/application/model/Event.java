1c7cca9f6681d8d83eed8968e21d2d872edb5249
Mariya_Russakova <Mariya_Russakova@epam.com>
2023-04-01 16:17:47 +0600

    update existing models

--- src/main/java/com/example/application/model/Event.java
+++ src/main/java/com/example/application/model/Event.java
@@ -1,17 +1,28 @@
 package com.example.application.model;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name = "event")
 @Data
 @Builder(toBuilder = true)
 @NoArgsConstructor
 @AllArgsConstructor
 public class Event {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private long id;
   private String title;
   private LocalDate date;
+  private BigDecimal ticketPrice;
 }
--- src/main/java/com/example/application/model/User.java
+++ src/main/java/com/example/application/model/User.java
@@ -1,15 +1,24 @@
 package com.example.application.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name = "user")
 @Data
 @Builder(toBuilder = true)
 @NoArgsConstructor
 @AllArgsConstructor
 public class User {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private long id;
   private String name;
   private String email;
